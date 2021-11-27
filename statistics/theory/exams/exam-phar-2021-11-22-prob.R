d63c7e856a763dd72e5c71507f00ea997f3c8872
Alfredo Sánchez Alberca <asalber@gmail.com>
2021-11-27 12:01:55 +0100

    Added Pharmacy exam 2021-11-22.

---
+++ statistics/theory/exams/exam-phar-2021-11-22-prob.R
@@ -0,0 +1,36 @@
+library("knitr")
+library("exams")
+
+setwd("statistics/theory/exams")
+
+questions <- c("vac-17-med", "vad-15-far", "prob-11-med", "test-9-med")
+
+exams2pdf(questions, n=1, name = c("exam-phar-2021-11-22-prob", "solution-phar-2021-11-22-prob"), encoding = "UTF-8",
+  dir = "pdf",
+  edir = "../questions",
+  template = c("templates/examceu.tex", "templates/solutionceu.tex"),
+  header = list(
+    Subject = "STATISTICS\\\\[2mm] (PROBABILITY AND RANDOM VARIABLES)",
+    Degree= "Pharmacy/Biotechnology 1st year",
+    Date = "November 22, 2021",
+    Version = "A",
+    Time = "1 hour"
+  )
+)
+
+# Remove directory of exam images
+unlink("img/exam1", recursive = TRUE) 
+exams2pandoc(questions, n=1, 
+             name = "exam-phar-2021-11-22-prob", 
+             encoding = "UTF-8", 
+             type="latex",
+             dir = "latex",
+             edir = "../questions",
+             sdir = "img",
+             template = c("templates/plain.tex"),
+             svg=T
+)
+
+pandoc("latex/exam-phar-2021-11-22-prob1.latex", format="markdown")
+
+
---
+++ statistics/theory/exams/latex/exam-phar-2021-11-22-prob1.markdown
@@ -0,0 +1,7 @@
+1.  **Question**\ 
+
+2.  **Question**\ 
+
+3.  **Question**\ T
+
+4.  **Question**\ 
